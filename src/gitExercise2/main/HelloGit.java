b99acb7b2a259ded30ccd755cb89060c28d8abe9
Gabriele Groppo <g.groppo@campus.unimib.it>
2024-10-22 15:56:53 +0200

    Resetto tutto come all'inizio

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -3,10 +3,5 @@ package gitExercise2.main;
 public class HelloGit {
 	public static void main (String [] args) {
 		System.out.println("Hello git!!!");
-<<<<<<< HEAD
-		System.out.println("Questa è una stampa!");
-=======
-		System.out.println("Questa è una nuova stampa!");
->>>>>>> 71c7b3a (Sono il secondo developer e ho modificato una riga di codice)
 	}
 }
